fb3d41dd874fc33d3dcb11522c3282e7d4531120
DavidMoralesVega <moralesvegadavid@gmail.com>
2025-08-02 12:53:59 -0400

    second

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -16,23 +18,28 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +(process.env.DB_PORT || 5432),
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD,      
       autoLoadEntities: true,
       synchronize: true,
     }),
 
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
+      rootPath: join(__dirname,'..','public'), 
     }),
 
     ProductsModule,
+
     CommonModule,
+
     SeedModule,
+
     FilesModule,
+
     AuthModule,
+
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,10 +14,19 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-    }),
+    })
   );
 
-  await app.listen(process.env.PORT || 3000);
-  logger.log(`App running on port ${process.env.PORT}`);
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('Teslo shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${ process.env.PORT }`);
 }
 bootstrap();
